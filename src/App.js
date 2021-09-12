b5a9e6ef9da9bdfacac24bf21000991cb0a4f12c
yuttana-du <yuttana.du@gmail.com>
2021-09-12 17:09:52 +0700

    Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import Newplace from "./places/pages/NewPlace";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-    
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact={true}>
+          <Newplace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Newplace =() =>{
+    return <h2>New place</h2>
+};
+
+export default Newplace;
