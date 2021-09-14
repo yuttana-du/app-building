fdf9218580611742ef2aa26b9ebbe9345a0da966
yuttana-du <yuttana.du@gmail.com>
2021-09-14 15:08:45 +0700

    addPlaceinput

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Newplace =() =>{
-    return <h2>New place</h2>
+import Input from "../../shared/components/FormElements/input";
+import "./NewPlace.css";
+
+const Newplace = () => {
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  );
 };
 
 export default Newplace;
--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -4,18 +4,16 @@ import "./Map.css";
 
 const Map = (props) => {
   const mapRef = useRef();
-const {center,zoom} = props;
+  const { center, zoom } = props;
 
-    useEffect(()=>{
-        const map = new window.google.maps.Map(mapRef.current, {
-            center: center,
-            zoom: zoom,
-          });
-        
-          new window.google.maps.Marker({ position: center, map: map });
-         
-    },[center,zoom]);
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom,
+    });
 
+    new window.google.maps.Marker({ position: center, map: map });
+  }, [center, zoom]);
   return (
     <div
       ref={mapRef}
